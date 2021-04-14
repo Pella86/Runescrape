61b445b4291bb119b00851ecffedb5d2a8dc67f9
Pella86 <pigmeo127@gmail.com>
2021-04-14 15:19:27 +0200

    Improved the list box for the champion names

--- GUI.py
+++ GUI.py
@@ -35,25 +35,27 @@ class StoneFrame:
         else:
             img_file_mod = img_file_mod + "_modified_inactive.png"
             
-        
+        # if the picture was already modified it loads it from the ./image 
+        # folder
         if pathlib.Path(img_file_mod):
             img = PIL.Image.open(img_file_mod)
         else:
-            
             # resize the image
             img = img.resize((img_size,img_size), PIL.Image.ANTIALIAS) 
             
+            # if the stone is inactive it makes it gray
             if not stone.active:
                 # Converts to gray the image
                 img = img.convert("LA")
                 
-                # Converts the black and white image to RGBA again to modify the
-                # alpha channel
+                # Converts the black and white image to RGBA again to modify 
+                # the alpha channel
                 img = img.convert("RGBA")
                 
                 # make the outer ring transparent
                 pixdata = img.load()
-    
+                
+                # every pixel outside the radius are now transparent
                 width, height = img.size
                 for x in range(width):
                     for y in range(height):
@@ -61,75 +63,73 @@ class StoneFrame:
                         if (x - radius)**2 + (y - radius)**2 > radius**2:
                             pixdata[x, y] = (255, 255, 255, 0)
             
+            # save the image for later
             img.save(img_file_mod)
-            
         
         
-        # prepare the images for the active and inactive stone
-        if stone.active:
-            img_file_mod = stone.img_file.replace(".png", "")
-            img_file_mod = img_file_mod + "_modified_active.png" 
-            
-            # check if the image was already 
-            if not pathlib.Path(img_file_mod).is_file():
-                
-                img = PIL.Image.open(stone.img_file)
-        
-                # make the image 50x50 size
-                img = img.resize((img_size,img_size), PIL.Image.ANTIALIAS) 
-                
-                img.save(img_file_mod)
-
-            else:
-                 img =  PIL.Image.open(img_file_mod)      
-            
-        else:
-            img_file_mod = stone.img_file.replace(".png", "")
-            img_file_mod = img_file_mod + "_modified_inactive.png"             
-            
-            if not pathlib.Path(img_file_mod).is_file():
-                
-                img = PIL.Image.open(stone.img_file)
-        
-                # make the image 50x50 size
-                img = img.resize((img_size,img_size), PIL.Image.ANTIALIAS) 
-                
-                # Converts to gray the image
-                img = img.convert("LA")
-                
-                # Converts the black and white image to RGBA again to modify the
-                # alpha channel
-                img = img.convert("RGBA")
-                
-                # make the outer ring transparent
-                pixdata = img.load()
-    
-                width, height = img.size
-                for x in range(width):
-                    for y in range(height):
-                        radius = img_size / 2
-                        if (x - radius)**2 + (y - radius)**2 > radius**2:
-                            pixdata[x, y] = (255, 255, 255, 0)
-                            
-                            
-                img.save(img_file_mod)
-            
-            else:
-                 img =  PIL.Image.open(img_file_mod)             
-        
-
+        # loads the image in a tk friendly format
         stone_image = itk.PhotoImage(img)  
         
+        # creates the label holding the image
         label = tkinter.Label(self.main_frame, image=stone_image)
         label.image = stone_image
         label.pack()
         
+        
+class ChampionNameBox:
+    ''' Class that holds the list of champion names'''
+    
+    
+    def __init__(self, parent_frame, lb_callback):
+        # lb_callback is the function that reacts when is a name is pressed
+        self.name_list_box = tkinter.Listbox(parent_frame)
+        self.name_list_box.pack()
+        
+        self.name_list_box.bind("<<ListboxSelect>>", lambda e : lb_callback(e))   
+        
+        # stores the list in the box
+        self.name_idx = []
+        
+    def update_names(self, selection_list):
+        names_to_remove = []
+        
+        # removes the names that arent in the selection list
+        for name in self.name_idx:
+            if name not in selection_list:
+                names_to_remove.append(name)
+       
+        for name in names_to_remove:
+            self.remove_name(name)
+        
+        # adds the missing names
+        for name in selection_list:
+            if name not in self.name_idx:
+                self.add_name(name)
+
+    def add_name(self, name):
+        # appends at the end of the list box the name
+        idx = len(self.name_idx)
+        self.name_list_box.insert(idx, name)
+        
+        self.name_idx.append(name)
+    
+    def remove_name(self, name):
+        idx = self.name_idx.index(name)
+        
+        del self.name_idx[idx]
+        
+        self.name_list_box.delete(idx)
+        
+        
+        
 
 class RuneGroupFrame:
+    ''' This class holds a rune group'''
     
     
     def __init__(self, parent_frame, rune_group, is_fragment = False):
         
+        # make it bigger for the runes, and smaller for the fragments
         if is_fragment:
             height, width = 150, 125
         else:
@@ -137,7 +137,10 @@ class RuneGroupFrame:
             
         self.main_frame = tkinter.Frame(parent_frame, height=height, width=width)
         self.main_frame.grid_propagate(0)
-    
+        
+        
+        # if a keystone exists, which for the fragment doesnt, create
+        # a keystone frame
         if rune_group.keystone.rune_row:
         
             keystone = rune_group.keystone.get_stone()
@@ -145,39 +148,39 @@ class RuneGroupFrame:
             key_rune_frame.main_frame.grid(row = 0, column = 0)
             
         
-        
+        # for each row of stone create a frame containing the 3 stones
         for j, row in enumerate(rune_group.stone_rows):
             stonerow_frame = tkinter.Frame(self.main_frame)
             stonerow_frame.grid(row=j + 1, column=0)
             
             for i, stone in enumerate(row.get_stones()):
-            
                 stone_frame = StoneFrame(stonerow_frame, stone, is_fragment)
                 stone_frame.main_frame.grid(row=0, column=i)
-        
-        
-        
-
 
 class App:
-    
+    ''' Main App '''
     
     def __init__(self, root, champions_list):
         
+        # the list of all champions containing links
         self.champions_list = champions_list
         
         self.main_frame = tkinter.Frame(root)
         self.main_frame.pack()
         
         
-        # champion info, add a search box, connected to a list box that gets 
+        # champion info, a search box, connected to a list box that gets 
         # filtered as soon as people type
         # and the first selected will be shown
         
         champion_info_frame = tkinter.Frame(self.main_frame)
         champion_info_frame.grid(row=0, column=0)
         
-        # Entry
+        # Entry box for user input
+        
+        # string var that keeps track of user input
+        lb = tkinter.Label(champion_info_frame, text="Enter champion name")
+        lb.pack()       
         
         self.input_trace = tkinter.StringVar()
         self.input_trace.trace_add("write", lambda var_name, idk, mode : self.update_runes_trace(var_name, idk, mode))
@@ -185,28 +188,14 @@ class App:
         self.name_entry = tkinter.Entry(champion_info_frame, textvariable=self.input_trace)
         self.name_entry.pack()
         
-        button = tkinter.Button(champion_info_frame, text="Search", command=lambda : self.update_runes())
-        button.pack()
+        # This will hold the list of champions
+        lb = tkinter.Label(champion_info_frame, text="Champions List")
+        lb.pack()
         
-        # this will be a search box
-        # name_label = tkinter.Label(champion_info_frame, text=champion_page.champion.name)
-        # name_label.pack()
         
-        # role_label = tkinter.Label(champion_info_frame, text=champion_page.role)
-        # role_label.pack()
-        
-        self.name_list_box = tkinter.Listbox(champion_info_frame)
-        self.name_list_box.pack()
-        
-        self.name_list_box.bind("<<ListboxSelect>>", lambda e : self.champ_selection(e))
-        
-        for i, name in enumerate(self.champions_list):
-            self.name_list_box.insert(i, name)
-            
-        self.name_list_box.selection_set(0)
+        self.name_list_box = ChampionNameBox(champion_info_frame, self.champ_selection)
+        self.name_list_box.update_names(self.champions_list.keys())
 
-            
-        
         # runes page shown
         self.runes_info_frame = tkinter.Frame(self.main_frame)
         self.runes_info_frame.grid(row=0, column=1)
@@ -254,6 +243,9 @@ class App:
         # cp = ChampionPage.ChampionPage(champion, champion.roles[0])        
         
         # self.show_runes(cp)
+        
+    def update_list_box(self, champion_list):
+        pass
         
     
     def show_options(self, champion): 
@@ -321,15 +313,17 @@ class App:
         user_input = self.input_trace.get()
         selection_list = self.match_champion(user_input)
         
-        # empty the list
-        self.name_list_box.delete(0, tkinter.END)
+        self.name_list_box.update_names(selection_list)
         
-        for i, name in enumerate(selection_list):
-            self.name_list_box.insert(i, name)
+        # # empty the list
+        # self.name_list_box.delete(0, tkinter.END)
+        
+        # for i, name in enumerate(selection_list):
+        #     self.name_list_box.insert(i, name)
             
-        if selection_list:
-            self.name_list_box.selection_set(0)
-            self.show_runes_name(selection_list[0])    
+        # if selection_list:
+        #     self.name_list_box.selection_set(0)
+        #     self.show_runes_name(selection_list[0])    
     
     def match_champion(self, user_input):
         selection_list =[]
@@ -346,15 +340,17 @@ class App:
         
         selection_list = self.match_champion(user_input)
         
-        # empty the list
-        self.name_list_box.delete(0, tkinter.END)
+        self.name_list_box.update_names(selection_list)
         
-        for i, name in enumerate(selection_list):
-            self.name_list_box.insert(i, name)
+        # # empty the list
+        # self.name_list_box.delete(0, tkinter.END)
+        
+        # for i, name in enumerate(selection_list):
+        #     self.name_list_box.insert(i, name)
             
-        if selection_list:
-            self.name_list_box.selection_set(0)
-            self.show_runes_name(selection_list[0])
+        # if selection_list:
+        #     self.name_list_box.selection_set(0)
+        #     self.show_runes_name(selection_list[0])
 
         
     def show_runes(self, champion_page):
@@ -376,7 +372,7 @@ class App:
         rg_frame.main_frame.grid(row=0, column=1)       
         
         rg_frame = RuneGroupFrame(self.rune_set_frame, rg3, is_fragment=True)
-        rg_frame.main_frame.grid(row=0, column=2)              
+        rg_frame.main_frame.grid(row=1, column=0, columnspan=2)              
         
         
         
